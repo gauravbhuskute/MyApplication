ca35aec25b8506dd590affd94f40a5e9f5e2dc30
gauravbhuskute <48159041+gauravbhuskute@users.noreply.github.com>
2019-08-22 11:23:55 +0530

    Update App.java

--- Practice/src/app/App.java
+++ Practice/src/app/App.java
@@ -9,7 +9,7 @@ public class App {
         System.out.println("Now start training");
         System.out.println("This is first day");
         System.out.println("This is second day");
-
+        System.out.println("Go home");
         System.out.println("Do not bunk the training");
 
     }
